4adf96697fed36dbb61d8aac28fc953014f83f13
ArjunQBTech <arjun.sharma@quadbtech.com>
2024-07-10 11:53:58 +0530

    Utility Functions added

--- src/nft-staking-rentspace-frontend/src/utils/utils.js
+++ src/nft-staking-rentspace-frontend/src/utils/utils.js
@@ -1,5 +1,48 @@
-export function formatMetadata(){
-    const str="{\22name\22:\22Minimalistic Villas#0\22,\22mimeType\22:\22image\22,\22url\22:\22https://cf-assets.yuku.app/BatchMint/Minimalistic/0.png\22,\22thumb\22:\22https://cf-assets.yuku.app/BatchMint/Minimalistic/0_thumb.png\22,\22description\22:\22This is the NFT from Minimalistic Villas collection\22,\22attributes\22:[{\22trait_type\22:\22Common\22,\22value\22:\22Common\22}]}"
-    let val=str.split("\22").join('"')
+import {principalToAccountIdentifier} from '@dfinity/ledger-icp'
+import { Principal } from '@dfinity/principal';
+import {Buffer} from 'buffer'
+
+export const convertPrincipalToAccountIdentifier = async (principal) => {
+  const accountIdentifier = await principalToAccountIdentifier(principal)
+  return accountIdentifier
+}
+
+
+export function formatMetadata(str){
+    let val=str.split("22").join('')
+    val=val.split('\\').join('"')
+    console.log(val)
     console.log(JSON.parse(val))
+    return JSON.parse(val)
+}
+
+function encodeTokenIdentifier(canisterId, tokenIndex) {
+  const tds = Buffer.from([10, 116, 105, 100]);
+  // Convert Canister ID to its byte representation
+  const principal = Principal.fromText(canisterId);
+  const canisterBytes = Buffer.from(principal.toUint8Array());
+
+  // Encode TokenIndex as a byte array in big-endian order
+  const indexBuffer = Buffer.alloc(4);
+  indexBuffer.writeUInt32BE(tokenIndex, 0);
+
+  // Concatenate tds, canisterBytes, and indexBuffer
+  const combinedBuffer = Buffer.concat([tds, canisterBytes, indexBuffer]);
+
+  // Convert the resulting byte array back to a principal
+  const tokenIdentifierPrincipal = Principal.fromUint8Array(combinedBuffer);
+  
+  // Convert the principal to text
+  return tokenIdentifierPrincipal.toText();
+}
+
+export function tokenIndexToTokenIdentifier(tokenIdx) {
+  const canID=process.env.DFX_NETWORK === "ic"?"2cwjm-cyaaa-aaaap-ahi3q-cai":"be2us-64aaa-aaaaa-qaabq-cai"
+  return encodeTokenIdentifier(canID, tokenIdx);
+}
+
+export function convertPointstoICP(points) {
+  // convert points from bigint to int
+  let pointsInt = Number(points)
+  return pointsInt * 0.01;
 }
