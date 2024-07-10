a9072253ceac8033dbe2bba483d322eaf8ec25cf
vimlesh <vkg911858@gmail.com>
2024-07-10 13:59:38 +0530

    toggle added on walletConnect btn

--- src/nft-staking-rentspace-frontend/src/Components/Navbar/Navbar.jsx
+++ src/nft-staking-rentspace-frontend/src/Components/Navbar/Navbar.jsx
@@ -3,6 +3,16 @@ import './Navbar.css'
 
 const Navbar = () => {
   const [navItems, setItems]=useState(['Leaderboard', 'FAQ', 'My Worlds']);
+  const [isConnected, setConnected]= useState(false);
+
+
+  function ConnectWallet(){
+    // logic for wallet connection and get response
+     const response = !isConnected     // demo value
+
+    setConnected(response)
+  }
+
   return (
     <div className='navbar-mainCont'>
     <div className='navbar-cont'>
@@ -16,8 +26,8 @@ const Navbar = () => {
 
       <section className='connectBtn-mainCont'>
          <div className='connectBtn-cont'>
-         <div className='btn1'> Connected </div>
-         <div className='btn2'> <span>Connect Wallet </span></div>
+         <div className='btn1'>{ isConnected ? 'Connected' : '' }</div>
+         <div onClick={ConnectWallet} className='btn2 ' style={isConnected ? { right:0 } : { }}> <span>Connect Wallet </span></div>
          </div>
           
       </section>
