2ac9fb2ae86191ec0dcdf2453d2e6a6e23814756
ArjunQBTech <arjun.sharma@quadbtech.com>
2024-07-10 11:41:34 +0530

    Added Test for Update User

--- src/Tests/userTests.sh
+++ src/Tests/userTests.sh
@@ -3,3 +3,6 @@ canister="nft-staking-rentspace-backend"
 dfx canister call $canister createUser '(record {name="Arjun Sharma"; email="jayshrma161@gmail.com"})'
 
 dfx canister call $canister getUser
+
+dfx canister call $canister updateUserProfile '(record {email= opt "ee@gmail.com"})'
+
