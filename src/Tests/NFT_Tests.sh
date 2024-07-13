ffcb58c718e18ce38dd7c67182dd0a939c2da95d
ArjunQBTech <arjun.sharma@quadbtech.com>
2024-07-13 14:52:32 +0530

    Tests for stakeNFT demo addded

--- src/Tests/NFT_Tests.sh
+++ src/Tests/NFT_Tests.sh
@@ -1,10 +1,10 @@
 canister="nft-staking-rentspace-backend"
-# account_id=$(dfx ledger account-id | grep -oP '(?<=\")(.*?)(?=\")')
+account_id=$(dfx ledger account-id | grep -oP '(?<=\")(.*?)(?=\")')
 
-# echo "'${account_id}'"
+echo "'${account_id}'"
 
-# dfx canister call $canister importNFTs '("c5822914fbb69c6c0fd4e5d0f3d8f0ad8ef78bc27384965009a3f97f39436294")'
+dfx canister call $canister importNFTs '("c5822914fbb69c6c0fd4e5d0f3d8f0ad8ef78bc27384965009a3f97f39436294")'
 
-# dfx canister call $canister getUserNFTs
+dfx canister call $canister getUserNFTs
 
 dfx canister call $canister stakeNFT '("hakfb-pqkor-uwjaa-aaaaa-aeaaa-eaqca-aaaag-a")'
