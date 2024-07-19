c60b9705d5eb66a9f270b717ba1832f3b892bc75
ArjunQBTech <arjun.sharma@quadbtech.com>
2024-07-19 11:22:21 +0530

    New CheckBoxCard Component

---
+++ src/nft-staking-rentspace-frontend/src/Components/Card/CheckBoxCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Card.css';
+
+const CheckCard = ({ id, imgURL, name, desc, handleChange }) => {
+  const handleCheck = (e) => {
+    handleChange(e.target.checked, id);
+  };
+
+  return (
+    <div className="card">
+      <input onClick={handleCheck} id={id} type='checkbox' className='checkbox' />
+      <img src={imgURL} className='card-image' alt={name} width={418} />
+      <div className="card-body">
+        <h2 className="card-title">{name}</h2>
+        <p className="card-content">{desc}</p>
+      </div>
+    </div>
+  );
+};
+
+export default CheckCard;
