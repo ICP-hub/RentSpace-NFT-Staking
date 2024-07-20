2ce069a01c893b789897e45d0dfbba9eabe9c309
vimlesh <vkg911858@gmail.com>
2024-07-20 17:36:21 +0530

    bug fixed

--- src/nft-staking-rentspace-frontend/src/Components/Navbar/Navbar.jsx
+++ src/nft-staking-rentspace-frontend/src/Components/Navbar/Navbar.jsx
@@ -37,14 +37,12 @@ const Navbar = () => {
 
       <section className='connectBtn-mainCont'>
          <div className='connectBtn-cont'>
-         <div className='btn1'>{ isConnected ? 'Connected' : '' }</div>
-<<<<<<< HEAD
+         <div className='btn1'>{ isConnected ? 'Connected' : '' }</div> 
          <div  className='btn2 ' style={isConnected ? { right:0 } : { }}>
           { isConnected ? <button onClick={handleLogout}> Logout </button> : <button onClick={ ConnectWallet } >Connect Wallet </button> }
            </div>
-=======
          <div onClick={ConnectWallet} className='btn2 ' style={isConnected ? { right:0 } : { }}> <span>{isConnected ? 'Logout' : 'Connect Wallet'} </span></div>
->>>>>>> df1d209c6dc42e8070448a4639b043e2dc7762a3
+
          </div>
           
       </section>
