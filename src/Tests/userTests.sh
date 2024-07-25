b060c021f6adf021497e2ba1d59dac6a69329542
VimleshG1 <vimlesh.gupta@quadbtech.com>
2024-07-25 13:07:38 +0530

    Update userTests.sh

--- src/Tests/userTests.sh
+++ src/Tests/userTests.sh
@@ -3,10 +3,13 @@ aid="user1"  # Example aid
 points=1000 # Example value
 
 
+echo "---------Creating new user----------"
 dfx canister call $canister createUser '(record {name="Arjun Sharma"; email="jayshrma161@gmail.com"})'
 
+echo "---------get user----------"
 dfx canister call $canister getUser
 
+echo "---------updating user profile----------"
 dfx canister call  $canister updateUserProfile '(record {name = null; email = opt "ee@gmail.com"})'
 
 echo "---------all tokens for user----------"
