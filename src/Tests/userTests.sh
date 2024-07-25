9848ae0e8009e94cf6e43ba4abbc3c13a5e24643
VimleshG1 <vimlesh.gupta@quadbtech.com>
2024-07-25 12:40:49 +0530

    Update test scripts

--- src/Tests/userTests.sh
+++ src/Tests/userTests.sh
@@ -1,8 +1,19 @@
 canister="nft-staking-rentspace-backend"
+aid="user1"  # Example aid 
+points=1000 # Example value
+
 
 dfx canister call $canister createUser '(record {name="Arjun Sharma"; email="jayshrma161@gmail.com"})'
 
 dfx canister call $canister getUser
 
-dfx canister call $canister updateUserProfile '(record {email= opt "ee@gmail.com"})'
+dfx canister call  $canister updateUserProfile '(record {name = null; email = opt "ee@gmail.com"})'
+
+echo "---------all tokens for user----------"
+dfx canister call $Canister getAllUserTokens "(\"${aid}\")"
+
+echo "---------Claiming points for user----------"
+dfx canister call $Canister claimPoints "( ${points} : nat )"
+
+
 
