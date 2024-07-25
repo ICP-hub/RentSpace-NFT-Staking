5fa63a4a5d2fba11a6fbd5fa0e88a714062fb4c9
VimleshG1 <vimlesh.gupta@quadbtech.com>
2024-07-25 13:00:46 +0530

    Update test scripts

--- src/Tests/NFT_Tests.sh
+++ src/Tests/NFT_Tests.sh
@@ -1,10 +1,31 @@
 canister="nft-staking-rentspace-backend"
 account_id=$(dfx ledger account-id | grep -oP '(?<=\")(.*?)(?=\")')
+nftID= "hakfb-pqkor-uwjaa-aaaaa-aeaaa-eaqca-aaaag-a"   # Example nftID, we can change it 
+tokenData="c5822914fbb69c6c0fd4e5d0f3d8f0ad8ef78bc27384965009a3f97f39436294"  # Example  tokenData, we can change it 
 
 echo "'${account_id}'"
 
-dfx canister call $canister importNFTs '("c5822914fbb69c6c0fd4e5d0f3d8f0ad8ef78bc27384965009a3f97f39436294")'
+echo "---------Getting all staked NFTs----------"
+dfx canister call $Canister getAllStaked
 
+echo "---------Getting points accumulated----------"
+dfx canister call $Canister getPointsAccumulated "(\"${nftId}\")"
+
+echo "---------Importing NFTs----------"
+dfx canister call $Canister importNFTs "(${tokenData})"
+
+echo "---------Getting User NFTs----------"
 dfx canister call $canister getUserNFTs
 
-dfx canister call $canister stakeNFT '("hakfb-pqkor-uwjaa-aaaaa-aeaaa-eaqca-aaaag-a")'
+echo "---------Getting imported NFTs for user----------"
+dfx canister call $Canister getUserImportedNFTs
+
+echo "---------Getting staked NFTs for user----------"
+dfx canister call $Canister getUserStakedNFTs
+
+echo "---------Staking NFT for user----------"
+dfx canister call $Canister stakeNFT "(\"${nftId}\")"
+
+echo "---------Unstaking NFT for user----------"
+dfx canister call $Canister unstakeNFT "(\"${nftId}\")"
+
